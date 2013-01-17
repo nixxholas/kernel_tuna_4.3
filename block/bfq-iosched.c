dcb33060c99e10aeb0128304eda2acd03026bc80
Boy Petersen <boypetersen@gmail.com>
2013-01-17 18:04:36 +0100

    Added BFQv5 I/O Scheduler
    Signed-off-by: Amperific <amperific@gmail.com>
    
    Conflicts:
    	block/bfq-iosched.c

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -72,10 +72,17 @@ static const int bfq_fifo_expire[2] = { HZ / 4, HZ / 8 };
 static const int bfq_back_max = 16 * 1024;
 
 /* Penalty of a backwards seek, in number of sectors. */
+<<<<<<< HEAD
 static const int bfq_back_penalty = 1;
 
 /* Idling period duration, in jiffies. */
 static int bfq_slice_idle = 0;
+=======
+static const int bfq_back_penalty = 2;
+
+/* Idling period duration, in jiffies. */
+static int bfq_slice_idle = HZ / 125;
+>>>>>>> d32b000... Added BFQv5 I/O Scheduler
 
 /* Default maximum budget values, in sectors and number of requests. */
 static const int bfq_default_max_budget = 16 * 1024;
@@ -3017,7 +3024,11 @@ static int __init bfq_init(void)
 	/*
 	 * Can be 0 on HZ < 1000 setups.
 	 */
+<<<<<<< HEAD
 	if (bfq_slice_idle == 0 && HZ >= 1000)
+=======
+	if (bfq_slice_idle == 0)
+>>>>>>> d32b000... Added BFQv5 I/O Scheduler
 		bfq_slice_idle = 1;
 
 	if (bfq_timeout_async == 0)
