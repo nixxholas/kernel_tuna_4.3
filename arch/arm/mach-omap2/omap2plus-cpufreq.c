612c2a89150922e62a6cd763fedfa4731ab2b4a5
Boy Petersen <boypetersen@gmail.com>
2013-01-29 11:43:21 +0100

    removed OPP25 fuse, some cleaning

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -393,7 +393,7 @@ static int __cpuinit omap_cpu_init(struct cpufreq_policy *policy)
 	}
 
 	/* FIXME: what's the actual transition time? */
-	policy->cpuinfo.transition_latency = 30 * 1000;
+	policy->cpuinfo.transition_latency = 30000;
 
 	return 0;
 
