f15b6e53124ff2a0bff355159c58d2d6e20be6e4
Boy Petersen <boypetersen@gmail.com>
2013-05-28 13:52:34 +0200

    mm/vmscan.c: swappiness = 30
    ramdisk script will disable swappiness if zram is not enabled right after boot.
    
    value of 30 inspired by the default value Cyanogenmod sets upon enabling zram.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -148,11 +148,8 @@ struct scan_control {
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-#ifdef CONFIG_SWAP
-int vm_swappiness = 50;
-#else
-int vm_swappiness = 0;
-#endif
+
+int vm_swappiness = 30;
 long vm_total_pages;	/* The total number of pages which the VM controls */
 
 static LIST_HEAD(shrinker_list);
