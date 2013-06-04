3f4b2f35144789ea37278151f6bcc64f8af66e36
Boy Petersen <boypetersen@gmail.com>
2013-06-04 10:50:20 +0200

    ondemandplus: increase down_differential

--- drivers/cpufreq/cpufreq_ondemandplus.c
+++ drivers/cpufreq/cpufreq_ondemandplus.c
@@ -95,7 +95,7 @@ static unsigned long timer_rate;
 #define DEFAULT_UP_THRESHOLD 87
 static unsigned long up_threshold;
 
-#define DEFAULT_DOWN_DIFFERENTIAL 54
+#define DEFAULT_DOWN_DIFFERENTIAL 58
 static unsigned long down_differential;
 
 #define DEFAULT_INTER_HIFREQ 1036800
