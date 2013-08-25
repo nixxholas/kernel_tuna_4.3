f1a57a64d51bcf7bf5b6d1f59967c97f55c5f6ce
Francisco Franco <franciscofranco.1990@gmail.com>
2013-08-25 06:58:52 +0100

    soundcontrol: add a new version that talks directly with the HSGAIN hardware register to achieve higher Headphones volume.
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: Amperific <amperific@gmail.com>
    
    Conflicts:
    	drivers/misc/Makefile
    	drivers/misc/sound_control.c
    	sound/soc/codecs/twl6040.c

--- drivers/misc/sound_control.c
+++ drivers/misc/sound_control.c
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 /* drivers/misc/sound_control.c
  *
  * Copyright 2012  Ezekeel
+=======
+/*
+ * Copyright 2013 Francisco Franco
+>>>>>>> 29db45d... soundcontrol: add a new version that talks directly with the HSGAIN hardware register to achieve higher Headphones volume.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -10,6 +15,7 @@
 #include <linux/init.h>
 #include <linux/device.h>
 #include <linux/miscdevice.h>
+<<<<<<< HEAD
 #include <linux/sound_control.h>
 
 #define SOUNDCONTROL_VERSION 1
@@ -98,6 +104,38 @@ static ssize_t soundcontrol_volumeboost_write(struct device * dev, struct device
     else 
 	{
 	    pr_info("%s: invalid input\n", __FUNCTION__);
+=======
+
+#define SOUNDCONTROL_VERSION 1
+
+int hp_boost_val = 0;
+
+extern void soundcontrol_hp_boost(int vol_boost);
+
+/*
+ * Sysfs get/set entries
+ */
+
+static ssize_t volume_boost_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+    return sprintf(buf, "%d\n", hp_boost_val);
+}
+
+static ssize_t volume_boost_store(struct device * dev, struct device_attribute * attr, const char * buf, size_t size)
+{
+    int new_val;
+
+	sscanf(buf, "%d", &new_val);
+	
+	if (new_val < 0 || new_val > 1)
+		return -EINVAL;
+
+	if (new_val != hp_boost_val)
+	{
+		hp_boost_val = new_val;
+		soundcontrol_hp_boost(hp_boost_val);
+		pr_info("Sound Control new Headphones boost value %d\n", hp_boost_val);
+>>>>>>> 29db45d... soundcontrol: add a new version that talks directly with the HSGAIN hardware register to achieve higher Headphones volume.
 	}
 
     return size;
@@ -105,6 +143,7 @@ static ssize_t soundcontrol_volumeboost_write(struct device * dev, struct device
 
 static ssize_t soundcontrol_version(struct device * dev, struct device_attribute * attr, char * buf)
 {
+<<<<<<< HEAD
     return sprintf(buf, "%u\n", SOUNDCONTROL_VERSION);
 }
 
@@ -132,6 +171,34 @@ static struct miscdevice soundcontrol_device =
     };
 
 static int __init soundcontrol_control_init(void)
+=======
+    return sprintf(buf, "%d\n", SOUNDCONTROL_VERSION);
+}
+
+static DEVICE_ATTR(volume_boost, 0777, volume_boost_show, volume_boost_store);
+
+static DEVICE_ATTR(version, 0777 , soundcontrol_version, NULL);
+
+static struct attribute *soundcontrol_attributes[] = 
+{
+	&dev_attr_volume_boost.attr,
+	&dev_attr_version.attr,
+	NULL
+};
+
+static struct attribute_group soundcontrol_group = 
+{
+	.attrs  = soundcontrol_attributes,
+};
+
+static struct miscdevice soundcontrol_device = 
+{
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "soundcontrol",
+};
+
+static int __init soundcontrol_init(void)
+>>>>>>> 29db45d... soundcontrol: add a new version that talks directly with the HSGAIN hardware register to achieve higher Headphones volume.
 {
     int ret;
 
@@ -139,6 +206,7 @@ static int __init soundcontrol_control_init(void)
 
     ret = misc_register(&soundcontrol_device);
 
+<<<<<<< HEAD
     if (ret) 
 	{
 	    pr_err("%s misc_register(%s) fail\n", __FUNCTION__, soundcontrol_device.name);
@@ -148,11 +216,23 @@ static int __init soundcontrol_control_init(void)
 
     if (sysfs_create_group(&soundcontrol_device.this_device->kobj, &soundcontrol_notification_group) < 0) 
 	{
+=======
+    if (ret) {
+	    pr_err("%s misc_register(%s) fail\n", __FUNCTION__, soundcontrol_device.name);
+	    return 1;
+	}
+
+    if (sysfs_create_group(&soundcontrol_device.this_device->kobj, &soundcontrol_group) < 0) {
+>>>>>>> 29db45d... soundcontrol: add a new version that talks directly with the HSGAIN hardware register to achieve higher Headphones volume.
 	    pr_err("%s sysfs_create_group fail\n", __FUNCTION__);
 	    pr_err("Failed to create sysfs group for device (%s)!\n", soundcontrol_device.name);
 	}
 
     return 0;
 }
+<<<<<<< HEAD
 
 device_initcall(soundcontrol_control_init);
+=======
+late_initcall(soundcontrol_init);
+>>>>>>> 29db45d... soundcontrol: add a new version that talks directly with the HSGAIN hardware register to achieve higher Headphones volume.
